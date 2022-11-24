cf737d9113c4a00a69216c09cd5faaccdb3d1d50
yewinag <yewinaung831@gmail.com>
2022-11-24 16:07:54 +0700

    remove firebase

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,6 @@ import { theme } from "@utils";
 import { store, persistor } from "@redux";
 import { ModuleMain } from "@modules";
 import "@translations/i18n";
-import { requestFirebaseNotificationPermission } from "./firebaseInit";
-import { fireBaseAPI } from "./API/fireBase";
-
-// import "./assets/styled.less";
-
-requestFirebaseNotificationPermission()
-    .then((firebaseToken) => {
-        // eslint-disable-next-line no-console
-        // console.log("firebaseToken", firebaseToken);
-        const userInfo = store.getState().auth?.userInfo;
-
-        if (userInfo?._id && typeof firebaseToken === "string") {
-            if (userInfo?.notifSetting?.channels?.adminPanel)
-                fireBaseAPI.saveToken(userInfo._id, { notiToken: firebaseToken });
-            else fireBaseAPI.saveToken(userInfo._id, { notiToken: "" });
-        }
-    })
-    .catch((err) => {
-        // eslint-disable-next-line no-console
-        console.log("err", err);
-    });
 
 function App() {
     return (
